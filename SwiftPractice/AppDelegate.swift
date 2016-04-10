dbc8ca31e7b912c5ca04ef680c2553902af855c0
Patrick Pan <patrick_pan@epam.com>
2016-04-10 23:56:04 +0800

    Start project

--- SwiftPractice/AppDelegate.swift
+++ SwiftPractice/AppDelegate.swift
@@ -16,7 +16,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
+            let rootView: MainViewController = MainViewController()
+            if let window = self.window{
+                window.rootViewController = rootView
+            }
+        
         return true
     }
 

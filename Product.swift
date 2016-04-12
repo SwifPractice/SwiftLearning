2178b5e63c93bf9d8e8648d249e6cf390dfa2a53
Patrick Pan <patrick_pan@epam.com>
2016-04-12 18:03:55 +0800

    CustomizedTableCell.swift

---
+++ Product.swift
@@ -0,0 +1,44 @@
+import Foundation
+
+class Product {
+    var id : Int
+    var name : String?
+    var price : Double
+    var quantity : Int
+    var description : String?
+    var status : ProductStatus
+    
+    init() {
+        self.id = 0
+        self.name = nil
+        self.price = 0.0
+        self.quantity = 0
+        self.description = nil
+        self.status = .ToBuy
+    }
+    
+    init?(id: Int, name : String, price : Double, quantity : Int, description : String, status : ProductStatus) {
+        self.id = id
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+        self.description = description
+        self.status = status
+        
+        if id == 0 || name.isEmpty {
+            return nil
+        }
+    }
+    
+    static func getProductStatusString(status : ProductStatus) -> String? {
+        var statusString : String? = nil
+        switch status {
+        case .InProgress:
+            statusString = "In Progress"
+        case .ToBuy:
+            statusString = "To Buy"
+        }
+        
+        return statusString
+    }
+}
---
+++ ProductStatus.swift
@@ -0,0 +1,14 @@
+//
+//  ProductStatus.swift
+//  SwiftPractice
+//
+//  Created by EPCNSZXW0311 on 4/12/16.
+//  Copyright © 2016 EPCNSZXW0311. All rights reserved.
+//
+
+import Foundation
+
+enum ProductStatus {
+    case ToBuy
+    case InProgress
+}
